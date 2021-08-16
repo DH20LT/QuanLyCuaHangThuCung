aec90e3227f3351b491c30305d7239a6504f9a0f
Tạ Quang Khôi <45689286+TaQuangKhoi@users.noreply.github.com>
2021-08-16 19:03:53 +0700

    sắp xếp lại

---
+++ Cho.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace QuanLyCuaHangThuCung
+{
+    class Cho : Thu
+    {
+        int kichThuocLong;
+        public int getKichThuocLong() { return kichThuocLong; }
+        public void setKichThuocLong(int kichThuocLong) { this.kichThuocLong = kichThuocLong; }
+        public Cho()
+        {
+            this.setMaThu(0);
+            this.setLoaiThu(2);
+            this.setTenThu(" ");
+            this.setNguonGoc("");
+            this.setDonGia(0);
+            this.setSoLuong(0);
+            this.setKichThuocLong(0);
+        }
+        public Cho(int maThu, string tenThu, string nguonGoc, int donGia, int kichThuocLong, int soLuong)
+        {
+            this.setLoaiThu(2);
+            this.setMaThu(maThu);
+            this.setTenThu(tenThu);
+            this.setNguonGoc(nguonGoc);
+            this.setDonGia(donGia);
+            this.setSoLuong(soLuong);
+            this.setKichThuocLong(kichThuocLong);
+        }
+        public override void tiengKeu()
+        {
+            Console.WriteLine("Gau Gau");
+        }
+        public override void thucAn()
+        {
+            Console.WriteLine("Xuong");
+        }
+        public override void NhapThongTin()
+        {
+            base.NhapThongTin();
+            Console.WriteLine("Nhap Kich Thuoc Long: ");
+            kichThuocLong = int.Parse(Console.ReadLine());
+        }
+        public override void XuatThongTin()
+        {
+            base.XuatThongTin();
+            Console.WriteLine("Kich Thuoc Long: " + kichThuocLong);
+        }
+    }
+}
---
+++ Meo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace QuanLyCuaHangThuCung
+{
+    class Meo : Thu
+    {
+        string giongMeo; //AnDo, My, TayBanNha, Anh
+        public Meo()
+        {
+            this.setMaThu(0);
+            this.setLoaiThu(1);
+            this.setTenThu(" ");
+            this.setNguonGoc("");
+            this.setDonGia(0);
+            this.setSoLuong(0);
+            giongMeo = "";
+        }
+        public Meo(int maThu, string tenThu, string nguonGoc, int donGia, string giongMeo, int soLuong)
+        {
+            this.setLoaiThu(1);
+            this.setMaThu(maThu);
+            this.setTenThu(tenThu);
+            this.setNguonGoc(nguonGoc);
+            this.setDonGia(donGia);
+            this.setSoLuong(soLuong);
+            this.giongMeo = giongMeo;
+        }
+        public string getGiongMeo() { return giongMeo; }
+        public void setGiongMeo(string giongMeo) { this.giongMeo = giongMeo; }
+        public override void tiengKeu()
+        {
+            Console.WriteLine("Meo Meo");
+        }
+        public override void thucAn()
+        {
+            Console.WriteLine("Chuot");
+        }
+        public override void NhapThongTin()
+        {
+            base.NhapThongTin();
+            Console.WriteLine("Nhap Giong Meo: ");
+            giongMeo = Console.ReadLine();
+        }
+        public override void XuatThongTin() 
+        {
+            base.XuatThongTin();
+            Console.WriteLine("Giong Meo" + giongMeo);
+        }
+    }
+}
